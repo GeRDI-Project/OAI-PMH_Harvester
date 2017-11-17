137d831b535749297fabe3904eb4fe25ee0829c3
Robin Weiß <row@informatik.uni-kiel.de>
2017-11-17 12:27:04 +0100

    SAI-84 Created constants files and cleaned up the loadEntries() function

--- src/main/java/de/gerdiproject/harvest/harvester/OaipmhHarvester.java
+++ src/main/java/de/gerdiproject/harvest/harvester/OaipmhHarvester.java
@@ -19,7 +19,8 @@
 package de.gerdiproject.harvest.harvester;
 
 import de.gerdiproject.harvest.IDocument;
-import de.gerdiproject.harvest.OaipmhContextListener;
+import de.gerdiproject.harvest.oaipmh.constants.OaiPmhParameterConstants;
+import de.gerdiproject.harvest.oaipmh.constants.OaiPmhUrlConstants;
 import de.gerdiproject.json.datacite.*;
 import de.gerdiproject.json.datacite.Date;
 import de.gerdiproject.json.datacite.abstr.AbstractDate;
@@ -48,11 +49,6 @@ import java.util.*;
  */
 public class OaipmhHarvester extends AbstractListHarvester<Element>
 {
-    private enum METADATA_FORMATS {oai_dc, ore, mets, datacite3}
-
-    private final static String BASE_URL =  "%s?verb=ListRecords&resumptionToken=%s";
-    private final static String BASE_URL_FILTERED = "%s?verb=ListRecords%s%s&metadataPrefix=%s";
-
     private static final String PROVIDER = "PANGAEA";
     //private static final String PROVIDER_URL = "https://depositonce.tu-berlin.de";
 
@@ -70,8 +66,8 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
 
 
     /**
-     * Default Constructor. 
-     * 
+     * Default Constructor.
+     *
      */
     public OaipmhHarvester()
     {
@@ -86,11 +82,12 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
     {
         super.setProperty(key, value);
 
-        if (getProperty(OaipmhContextListener.PROPERTY_FROM ) != null 
-        		&& getProperty(OaipmhContextListener.PROPERTY_TO ) != null 
-        		&& key.equals(OaipmhContextListener.PROPERTY_FROM) || key.equals(OaipmhContextListener.PROPERTY_TO))
+        if (key.equals(OaiPmhParameterConstants.DATE_FROM_KEY) ||
+            key.equals(OaiPmhParameterConstants.DATE_TO_KEY) ||
+            key.equals(OaiPmhParameterConstants.METADATA_PREFIX_KEY))
             init();
     }
+
 
     /**
      * Grap stuff from URL
@@ -99,49 +96,68 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
     @Override
     protected Collection<Element> loadEntries()
     {
-        Collection<Element> docs = new LinkedList<>();
-        String hostUrl = getProperty(OaipmhContextListener.PROPERTY_HOST_URL);
-        String domainsUrl = String.format(
-        		BASE_URL_FILTERED, 
-        		hostUrl,
-        		getProperty(OaipmhContextListener.PROPERTY_FROM), 
-        		getProperty(OaipmhContextListener.PROPERTY_TO),
-        		getProperty(OaipmhContextListener.PROPERTY_METADATA_PREFIX));
+        Collection<Element> documentsList = new LinkedList<>();
+        String url = assembleMainUrl();
 
-        //logger.info("from " + getProperty(OaipmhContextListener.PROPERTY_FROM));
-        //logger.info("until " + getProperty(OaipmhContextListener.PROPERTY_TO));
-        //logger.info("domainsUrl: " + domainsUrl);
-        Document doc = httpRequester.getHtmlFromUrl(domainsUrl);
-        //logger.info("Document null?: " + (doc == null));
+        while (url != null) {
+            logger.info("resumptionUrl: " + url);
 
-        Element token = doc.select("resumptionToken").first();
-        String rtoken = null;
-        String domainsUrlresumption = null;
-        //TODO: error at https://depositonce.tu-berlin.de/oai/request?verb=ListRecords&resumptionToken=oai_dc////6200
+            Document doc = httpRequester.getHtmlFromUrl(url);
 
-        while (token != null) {
-            rtoken = token.text();
-            //logger.info(rtoken);
-            domainsUrlresumption = String.format(BASE_URL, hostUrl, rtoken);
-            //logger.info("ResumptionToken: " + rtoken);
-            //logger.info("URL: " + domainsUrlresumption);
-           
-            Document doc1 = httpRequester.getHtmlFromUrl(domainsUrlresumption);
-            if (doc1 != null) {
-            		docs.addAll(doc1.select("record"));
-            		token = doc1.select("resumptionToken").first();
-            }
-            else
-            		token = null;
-            
+            if (doc == null)
+                break;
+
+            // add records to list
+            documentsList.addAll(doc.select("record"));
+
+            // get next URL
+            Element token = doc.select("resumptionToken").first();
+            url = (token != null)
+                  ? assembleResumptionUrl(token.text())
+                  : null;
         }
-        return docs;
+        logger.info("Finished getting " + documentsList.size() + " entries!");
+        return documentsList;
+    }
+
+
+    private String assembleMainUrl()
+    {
+        String mainUrl = null;
+        String host = getProperty(OaiPmhParameterConstants.HOST_URL_KEY);
+
+        if (host != null) {
+            String from = getProperty(OaiPmhParameterConstants.DATE_FROM_KEY);
+            String until = getProperty(OaiPmhParameterConstants.DATE_TO_KEY);
+            String metadataPrefix = getProperty(OaiPmhParameterConstants.METADATA_PREFIX_KEY);
+
+            StringBuilder queryBuilder = new StringBuilder();
+
+            if (from != null && until != null) {
+                queryBuilder.append(OaiPmhUrlConstants.DATE_FROM_QUERY).append(from);
+                queryBuilder.append(OaiPmhUrlConstants.DATE_TO_QUERY).append(until);
+            }
+
+            if (metadataPrefix != null)
+                queryBuilder.append(OaiPmhUrlConstants.METADATA_PREFIX_QUERY).append(metadataPrefix);
+
+            mainUrl = String.format(OaiPmhUrlConstants.BASE_URL, host, queryBuilder.toString());
+        }
+
+        return mainUrl;
+    }
+
+
+    private String assembleResumptionUrl(String resumptionToken)
+    {
+        String host = getProperty(OaiPmhParameterConstants.HOST_URL_KEY);
+        return String.format(OaiPmhUrlConstants.RESUMPTION_URL, host, resumptionToken);
     }
 
     /*private static ResourceType createResourceType() //TODO: how to deal with that and other meta data formats like ore, mets, etc
     {
         ResourceType resourceType = new ResourceType("Whatever Data", ResourceTypeGeneral.Dataset);
-        
+
         return resourceType;
     }*/
 
@@ -153,7 +169,7 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
     @Override
     protected List<IDocument> harvestEntry(Element entry)
     {
-    		//each entry-node starts with record subelements are header and metadata
+        //each entry-node starts with record subelements are header and metadata
         //Example: https://www.cancerdata.org/oai?verb=ListRecords&from=2017-11-01&metadataPrefix=oai_dc
         DataCiteJson document = new DataCiteJson();
         // get header and meta data stuff for each record
@@ -162,7 +178,7 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
         Boolean deleted = children.first().attr("status").equals("deleted") ? true : false;
         //logger.info("Identifier deleted?: " + deleted.toString() + " (" + children.first().attr("status") + ")");
         Elements metadata = children.select("metadata");
-        
+
         List<WebLink> links = new LinkedList<>();
         List<RelatedIdentifier> relatedIdentifiers = new LinkedList<>();
         List<AbstractDate> dates = new LinkedList<>();
@@ -196,23 +212,25 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
         // get last updated
         String recorddate = headers.select("datestamp").first().text();
         Date updatedDate = new Date(recorddate, DateType.Updated);
-		dates.add(updatedDate);
+        dates.add(updatedDate);
 
         //check if Entry is "deleted"
         if (deleted) {
-        		document.setVersion("deleted");
-        		document.setIdentifier(mainIdentifier);
+            document.setVersion("deleted");
+            document.setIdentifier(mainIdentifier);
+
             // add dates if there are any
             if (!dates.isEmpty())
-            		document.setDates(dates);
-        		
-        		return Arrays.asList(document);
+                document.setDates(dates);
+
+            return Arrays.asList(document);
         }
-        
-        
+
+
         // get publication date
         Calendar cal = Calendar.getInstance();
         Elements pubdate = metadata.select("dc|date");
+
         try {
             cal.setTime(dateFormat.parse(pubdate.first().text()));
             document.setPublicationYear((short) cal.get(Calendar.YEAR));
@@ -224,41 +242,50 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
 
         // get resource types
         Elements dctypes = metadata.select("dc|type");
-        for (Element e : dctypes) {
+
+        for (Element e : dctypes)
             dctype.add(e.text());
-        }
+
         document.setFormats(dctype);
 
         // get creators
         Elements creatorElements = metadata.select("dc|creator");
+
         for (Element e : creatorElements) {
             Creator creator = new Creator(e.text());
             creators.add(creator);
         }
+
         document.setCreators(creators);
 
         // get contributors
         Elements contribElements = metadata.select("dc|contributor");
+
         for (Element e : contribElements) {
             Contributor contrib = new Contributor(e.text(), ContributorType.ContactPerson);
             contributors.add(contrib);
         }
+
         document.setContributors(contributors);
 
         // get titles
         Elements dctitles = metadata.select("dc|title");
+
         for (Element title : dctitles) {
             Title dctitle = new Title(title.text());
             titles.add(dctitle);
         }
+
         document.setTitles(titles);
 
         // get descriptions
         Elements descriptionElements = metadata.select("dc|description");
+
         for (Element descElement : descriptionElements) {
             Description description = new Description(descElement.text(), DescriptionType.Abstract);
             descriptions.add(description);
         }
+
         document.setDescriptions(descriptions);
 
         // get web links
@@ -270,6 +297,7 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
         // get identifier URLs
         Elements identEles = metadata.select("dc|identifier");
         int numidents = identEles.size();
+
         for (Element identElement : identEles) {
             WebLink viewLink = new WebLink(identElement.text());
             viewLink.setName("Identifier" + numidents);
@@ -280,14 +308,17 @@ public class OaipmhHarvester extends AbstractListHarvester<Element>
 
         // get keyword subjects
         Elements dcsubjects = metadata.select("dc|subject");
+
         for (Element subject : dcsubjects) {
             Subject dcsubject = new Subject(subject.text());
             subjects.add(dcsubject);
         }
+
         document.setSubjects(subjects);
 
         // parse references
         Elements referenceElements = metadata.select("DOI");
+
         for (Element doiRef : referenceElements) {
             relatedIdentifiers.add(new RelatedIdentifier(
                                        doiRef.text(),
