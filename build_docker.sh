bca8da58811aa6dd4bd82ad0f022fc6500fa0ce2
Jan Frömberg <jan.froemberg@tu-dresden.de>
2017-12-18 16:08:27 +0100

    Update the build script to make use of new Harvester-Lib.

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mvn install
-cp target/OAIPMH-HarvesterService_5.0.0-SNAPSHOT.war bin/build/harvester.war
+cp target/OAIPMH-HarvesterService_5.1.0-SNAPSHOT.war bin/build/harvester.war
 cd bin/build
-docker build -t op-harvester-container:5.0 .
+docker build -t op-harvester-container:5.1 .
 docker rm -f oaipmh_harvester
-docker run --name oaipmh_harvester -d -p 8080:8080 --net elasticsearchmapping_elk op-harvester-container:5.0
+docker run --name oaipmh_harvester -d -p 8080:8080 --net elasticsearchmapping_elk op-harvester-container:5.1
 docker ps
