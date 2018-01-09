f4d19eff032e32f10b43cc5f3df6125ab592549d
Jan Frömberg <jan.froemberg@tu-dresden.de>
2018-01-09 15:24:56 +0100

    SAI-84 Removed Maven Jetty in pom.xml * Dockerfile adoption * Removed local build scripts in root -> find them in scripts folder

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@
 
 FROM jetty:9.4.7-alpine
 
-COPY \/target\/*.war $JETTY_BASE\/webapps\/oaipmh.war
+COPY \/target\/*.war $JETTY_BASE\/webapps\/harvester.war
 
 EXPOSE 8080
--- build_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mvn install
-cp target/OAIPMH-HarvesterService_5.1.0-SNAPSHOT.war bin/build/harvester.war
-cd bin/build
-/usr/local/opt/glassfish/libexec/bin/asadmin redeploy --name harvester --contextroot "/harvester" harvester.war
